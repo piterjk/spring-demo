8478977125438eae95f168bfcb3e10748bcb5ffb
PiterJK <piterjk@naver.com>
2025-02-17 17:22:08 +0900

    Mocking Test

--- src/test/java/com/piterjk/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/piterjk/springbootdemo/SpringbootDemoApplicationTests.java
@@ -2,17 +2,15 @@ package com.piterjk.springbootdemo;
 
 import com.piterjk.springbootdemo.post.entity.Post;
 import com.piterjk.springbootdemo.post.repository.PostRepository;
-import com.piterjk.springbootdemo.post.service.PostService;
 import com.piterjk.springbootdemo.post.service.impl.PostServiceImpl;
 import com.piterjk.springbootdemo.users.entity.User;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -20,9 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 //@Disabled  // 전체 테스트 클래스 비활성화
-@SpringBootTest
+//@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class SpringbootDemoApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(SpringbootDemoApplicationTests.class);
 
     @Mock
     private PostRepository postRepository;
@@ -30,13 +30,17 @@ class SpringbootDemoApplicationTests {
     @InjectMocks
     private PostServiceImpl postService;
 
+
     @Test
     void contextLoads() {
-        when(postRepository.findById(1L)).thenReturn(Optional.of(new Post(1L,"1234","q2343",new User())));
+       when(postRepository.findById(1L)).thenReturn(Optional.of(new Post(1L,"1234","q2343",new User())));
 
        Optional<Post> post = postService.findById(1L);
 
-        post.ifPresent(value -> assertEquals("1234", value.getTitle()));
+       post.ifPresent(value ->{
+           logger.debug("title " + value.getTitle());
+           assertEquals("1234", value.getTitle());
+       });
 
     }
 
